7a91da57b1bfbbee89e6ca0034be900561f94046
jianda <jianda851013@gmail.com>
2022-01-02 22:21:19 +0800

    [change] add front end docker file

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+module.exports = (app) => {
+  app.get('/', function (req, res, next) {
+    res.render('index', { title: '寶可夢集集樂' });
+  });
+  app.get('/users', function (req, res, next) {
+    res.send('respond with a resource');
+  });
+}
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '寶可夢集集樂' });
-});
-
-module.exports = router;
